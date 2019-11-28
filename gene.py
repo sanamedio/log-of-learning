b4783c8163d171b9ed3ee76c2ba3b3de58ef9825
l0k3ndr <lokendra.sharma.one@gmail.com>
2019-11-29 03:07:14 +0530

    fixed ordering

--- gene.py
+++ gene.py
@@ -7,17 +7,19 @@ mds = [ md for md in os.listdir("./notes/") if md.endswith(".md") ]
 
 mds.sort(key = lambda date: datetime.strptime(date.split(".")[0].split("notes-")[1], '%d-%b-%Y')) 
 
+print(mds)
+
 result = []
 k = 1
 for md in mds:
-
     data = open("./notes/"+md).readlines()
 
-
+    temp_res = []
     for line in data:
         if re.search("### \d+ - ", line.strip()):
-            result += [ ("|"+ str(k)  + "|["+line.strip().split(" - ",1)[1] + "](./notes/"+ md +") |")]
+            temp_res += [ ("|["+line.strip().split(" - ",1)[1] + "](./notes/"+ md +") |")]
             k = k+1
+    result += list(reversed(temp_res))
 
 
 
@@ -29,9 +31,9 @@ prefix = """
 
 ### """+ str(k-1) + """/100000
 
-| ID  | NAME |
-| ------------- | ------------- |
+| SNO | LINK |
+|------------- |
 """
 
 
-print (prefix + "\n".join(list(reversed(result))))
+print (prefix + "\n".join(list(reversed([ "|" + str(i+1)+"|" + result[i] for i in range(len(result))]))))
