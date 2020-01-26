cefd48a4bcb8f02f47b3f4b4fbe50369001f3ba4
Lokendra <8777623+l0k3ndr@users.noreply.github.com>
2020-01-26 20:11:29 +0530

    auto-update

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ git pull
 python3 readme_generator.py > README.md
 git add --all
 git commit -m "auto-update"
-git push
+git push --force
--- work.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def work_on():
-    path = os.getcwd()
-    print(f'Working on {path}')
-    return path
-
